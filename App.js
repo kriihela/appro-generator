f626fac04508c4c175320fc9cddf7ec15b22150a
Kristian Riihela <kristian.riihela@gmail.com>
2022-12-09 19:35:32 +0200

    tietokanta lisätty

--- App.js
+++ App.js
@@ -3,10 +3,23 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Details from './components/Details';
 import Results from './components/Results';
 import Info from './components/Info';
+import Favorites from './components/Favorites';
+import * as SQLite from 'expo-sqlite';
+import { useEffect } from 'react';
 
+const db = SQLite.openDatabase('favorite_places.db');
 const Stack = createNativeStackNavigator();
 
 export default function App() {
+
+  useEffect (() => {
+    db.transaction(tx => {
+      tx.executeSql(
+          'create table if not exists favorites (id integer primary key not null, name text, address text);'
+      );
+  });
+  }, []);
+
 
   return (
     <NavigationContainer>
@@ -20,6 +33,7 @@ export default function App() {
         <Stack.Screen name='Details' component={Details} />
         <Stack.Screen name='Results' component={Results} />
         <Stack.Screen name='Info' component={Info} />
+        <Stack.Screen name='Favorites' component={Favorites} />
       </Stack.Navigator>
     </NavigationContainer>
   );
