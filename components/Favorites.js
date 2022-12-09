36327df6b308e92293ea4c6cc9974570d3259af4
Kristian Riihela <kristian.riihela@gmail.com>
2022-12-09 19:34:44 +0200

    komponentti suosikkeja varten lisätty

---
+++ components/Favorites.js
@@ -0,0 +1,88 @@
+import React, { useState, useEffect } from 'react';
+import { View, FlatList, StyleSheet } from 'react-native';
+import { ListItem, Header, Icon } from 'react-native-elements';
+import * as SQLite from 'expo-sqlite';
+
+const db = SQLite.openDatabase('favorite_places.db');
+
+export default function Favorites({ route, navigation }) {
+
+  const [favorites, setFavorites] = useState([]);
+
+  // get
+  useEffect(() => {
+    db.transaction(tx => {
+      tx.executeSql('select * from favorites;', [], (_, { rows }) =>
+        setFavorites(rows._array)
+      );
+    });
+  }
+    , []);
+
+  // delete
+  const deletePlace = (item) => {
+    db.transaction(
+      tx => {
+        tx.executeSql(`delete from favorites where id = ?;`, [item.id]);
+      },
+      null,
+      updateList
+    )
+  }
+
+  // update
+  const updateList = () => {
+    db.transaction(tx => {
+      tx.executeSql('select * from favorites;', [], (_, { rows }) =>
+        setFavorites(rows._array)
+      );
+    });
+  }
+
+  return (
+    <View style={styles.container}>
+      <Header
+        containerStyle={{ backgroundColor: 'black', borderBottomColor: 'black' }}
+        leftComponent={<Icon name="arrow-back" color="white" onPress={() => navigation.navigate('Details')} />}
+        centerComponent={{ text: 'Favorites', style: { color: 'white', fontSize: 20 } }}
+      />
+      <FlatList
+        style={styles.list}
+        keyExtractor={item => item.id.toString()}
+        data={favorites}
+        renderItem={({ item }) => (
+          <ListItem bottomDivider style={styles.listItem}>
+            <ListItem.Content>
+              <ListItem.Title>{item.name}</ListItem.Title>
+              <ListItem.Subtitle>{item.address}</ListItem.Subtitle>
+            </ListItem.Content>
+            <Icon
+              name="delete"
+              size={30}
+              color="red"
+              onPress={() => deletePlace(item)}
+            />
+          </ListItem>
+        )}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'black',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  list: {
+    width: '100%',
+    backgroundColor: 'black',
+  },
+  listItem: {
+    backgroundColor: 'black',
+    borderBottomColor: 'black',
+    borderBottomWidth: 1,
+  },
+});
